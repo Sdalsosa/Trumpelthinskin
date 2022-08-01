2f5c92c85cc9392fb483c2a32aa7d3b645cb39df
Al <eganalan@gmail.com>
2022-08-01 21:29:46 +0000

    Add: - canvas to html and js files - styling to canvas - default background color

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+    const canvas = document.querySelector("canvas");
+    const cont = canvas.getContext("2d");
+    canvas.width = window.innerWidth/2;
+    canvas.height = window.innerHeight/2
+
+})
