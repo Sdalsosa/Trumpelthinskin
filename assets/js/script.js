e6f4a56a6a4b54da2b20207f2faffa692907781f
Al <eganalan@gmail.com>
2022-08-01 21:42:38 +0000

    Add classes needed template to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,44 @@ window.addEventListener("load", function() {
     canvas.width = window.innerWidth/2;
     canvas.height = window.innerHeight/2
 
+    // Detect key presses
+    class Movement {
+
+    }
+
+    // Trumpelthinskins weapon
+    class Speechbubble {
+        
+    }
+
+    // Trumpelthinskin sprite
+    class Trum {
+        
+    }
+
+    // CNN reporter sprite
+    class Cnn {
+        
+    }
+
+    // Front scenery images
+    class Foreground {
+        
+    }
+
+    // Background image
+    class Background {
+        
+    }
+
+    // Score counter and display
+    class Score {
+        
+    }
+
+    // Main game code
+    class Game {
+        
+    }
+
 })
