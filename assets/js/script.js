2d5a5e47196ac44166ed5eed6fe59deef1d6d4b9
Al <eganalan@gmail.com>
2022-08-01 23:39:37 +0000

    Add animation and character sprite
    Add:
    - character sprite to img folder
    - image to index.html
    - hide image in style.css using display:none
    - animate function to script.js
    - create character and game class in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,25 @@ window.addEventListener("load", function() {
     }
 
     // Trumpelthinskin sprite
-    class Trum {
+    class Trump {
+        constructor(game){
+            this.game = game;
+            this.image = document.getElementById('trumpSprite');
+            this.width = 250;
+            this.height= 175;
+            this.x = canvas.width - this.width - canvas.width/4;
+            this.y = canvas.height - this.height - canvas.height/10;
+            this.frameX = 0;
+        }
+
+        update(){
+            this.frameX < 11 ? this.frameX++ : this.frameX = 0;
+
+        }
         
+        draw(context){
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+        }
     }
 
     // CNN reporter sprite
@@ -41,7 +58,29 @@ window.addEventListener("load", function() {
 
     // Main game code
     class Game {
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.trump = new Trump(this);
+        }
+
+        update(){
+            this.trump.update();
+        }
         
+        draw(context){
+            this.trump.draw(context);
+        }
     }
 
+    const game = new Game(canvas.width, canvas.height);
+    // Animate the canvas
+    function animate() {
+        cont.clearRect(0, 0, canvas.width, canvas.height);
+        game.update();
+        game.draw(cont);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 })
